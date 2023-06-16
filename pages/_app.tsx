55c595a23ed1565f7c3d844e1d937504726963c6
acestar121 <ace.star.dev@outlook.com>
2023-06-16 04:49:50 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ function App({ Component, pageProps }: AppProps<{}>) {
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
       </QueryClientProvider>
     </div>
   );
