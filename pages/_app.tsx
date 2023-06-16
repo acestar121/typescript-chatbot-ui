e86f390dac1a5e54f3214e0fff4f69067c7cddb7
acestar121 <ace.star.dev@outlook.com>
2023-06-16 04:49:59 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
-
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
