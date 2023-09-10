4b8507f3c184dc4f53c251b5a912a04cefb395cc
acestar121 <ace.star.dev@outlook.com>
2023-09-10 19:49:51 -0400

    chat.ts - Error comment

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -3,7 +3,6 @@ import { OpenAIError, OpenAIStream } from '@/utils/server';
 
 import { ChatBody, Message } from '@/types/chat';
 
-// @ts-expect-error
 import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
 
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
